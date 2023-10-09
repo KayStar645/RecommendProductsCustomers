e18d876202631d258c025770762176ece45bcc7f
phatstran <hoangphat6302@gmail.com>
2023-10-09 22:51:15 +0700

    + AdminInterface

---
+++ RecommendProductsCustomers/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecommendProductsCustomers.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
