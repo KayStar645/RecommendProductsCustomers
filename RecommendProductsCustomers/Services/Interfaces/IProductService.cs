64fd8734b90ff131ddcc7c0ab5e8f9c3c36c449c
phatstran <hoangphat6302@gmail.com>
2023-10-16 19:28:02 +0700

    + View Product

---
+++ RecommendProductsCustomers/Services/Interfaces/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using RecommendProductsCustomers.Models;
+
+namespace RecommendProductsCustomers.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<List<ProductModel>> GetList();
+        Task<ProductModel> GetDetailByUserName(string pUserName);
+        Task<bool> Update(ProductModel pProduct);
+    }
+}
