98598f7c20606204840bed92383dbd7455e62b74
THUAN\thuan <thuanpt182@gmail.com>
2023-10-17 22:25:33 +0700

    hobby

---
+++ RecommendProductsCustomers/Controllers/Admin/HobbyController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecommendProductsCustomers.Models;
+using RecommendProductsCustomers.Services.Interfaces;
+
+namespace RecommendProductsCustomers.Controllers.Admin
+{
+    public class HobbyController : Controller
+    {
+        private readonly IHobbyService _hobbyService;
+
+        public HobbyController(IHobbyService hobbyService)
+        {
+            _hobbyService = hobbyService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index([FromQuery]string? pSearch = "")
+        {
+           ViewData["listHobbies"] = await _hobbyService.GetList(pSearch);
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] HobbyModel pHobbyModel)
+        {
+            bool flag = await _hobbyService.Create(pHobbyModel);
+            if (flag)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
